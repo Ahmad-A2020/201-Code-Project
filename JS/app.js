cbf922dac5f9cb9fd0a48ef7f68a1f29f23455ee
Ahmad-A92-3 <ahmad.alsyed92@gmail.com>
2021-03-29 16:40:12 +0300

    To submitt my lab 02

---
+++ JS/app.js
@@ -0,0 +1,84 @@
+'use strict'
+// This is for user name. 
+let userName=prompt("What is your name"); 
+alert("welcome to my website "+userName+" let us start ");
+
+// This is for geuss color 
+let myColor=prompt("is blue is my favirout color?");
+myColor=myColor.toLocaleLowerCase
+if (myColor=="yes" || "y") {
+    alert("you are wright");
+    console.log("you are correct my faviroyt color is Blue");
+    
+} else if(myColor=="no" || "n"){
+    alert("that is not correct, my favirout color is black ");
+    console.log("that is not correct, my favirout color is black ");
+    
+} else {
+    alert("you made wrong insert, any way my favirout color is Blue");
+    console.log("you made wrong insert, any way my favirout color is Blue");
+}
+
+// This is for geuss my age 
+let Age=prompt("is 28 is my age?");
+Age=Age.toLocaleLowerCase ;
+if (Age=="yes" || "y") {
+    alert("you are wright");
+    console.log("you are correct my age is 28");
+    
+} else if(Age=="no" || "n"){
+    alert("that is not correct, I am 28 year old ");
+    console.log("that is not correct,  I am 28 year old ");
+    
+} else {
+    alert("you made wrong insert, any way I am 28 year old ");
+    console.log("you made wrong insert, any way I am 28 year old ");
+}
+
+// This is for geuss my birthday place 
+let placeB=prompt("Do I born in Iraq?");
+placeB=placeB.toLocaleLowerCase
+if (placeB=="yes" || "y") {
+    alert("NO, that is wrong. I was born in Oman");
+    console.log("NO, that is wrong. I was born in Oman");
+    
+} else if(placeB=="no" || "n"){
+    alert("that is correct, I was born in Oman ");
+    console.log("that is correct, I was born in Oman ");
+    
+} else {
+    alert("you made wrong insert, anyway I was born in Oman ");
+    console.log("you made wrong insert, anyway I was born in Oman ");
+}
+
+// This is for jordan capital
+let Capital=prompt("is Amman Capital of Jordan?");
+Capital=Capital.toLocaleLowerCase
+if (Capital=="yes" || "y") {
+    alert("you are wright");
+    console.log("you are correct Amman is a capital of Jordan ");
+    
+} else if(Capital=="no" || "n"){
+    alert("that is not correct, Amman is a capital of Jordan");
+    console.log("that is not correct,  Amman is a capital of Jordan");
+    
+} else {
+    alert("you made wrong insert, anyway Amman is a capital of Jordan ");
+    console.log("you made wrong insert, anyway Amman is a capital of Jordan ");
+}
+
+// This is for month
+let month=prompt("do we in Ramadan?");
+month=month.toLocaleLowerCase
+if (month=="yes" || "y") {
+    alert("NO, that is wrong. Ramdan is the next month");
+    console.log("NO, that is wrong. Ramdan is the next month");
+    
+} else if(month=="no" || "n"){
+    alert("that is correct, Ramdan is the next month ");
+    console.log("that is correct,  Ramdan is the next month ");
+    
+} else {
+    alert("you made wrong insert, anyway Ramdan is the next month ");
+    console.log("you made wrong insert, anyway Ramdan is the next month ");
+}
